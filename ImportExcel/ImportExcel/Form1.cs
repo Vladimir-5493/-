22bd171409153227f7a47140bb4592ae46de9c7c
Vladimir-5493 <135590889+Vladimir-5493@users.noreply.github.com>
2023-06-25 15:02:51 +0700

    Add files via upload

---
+++ ImportExcel/ImportExcel/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace ImportExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadExcelFile(@"C:\Users\Ghost\source\repos\ImportExcel\ImportExcel\bin\Debug\Выходы.xlsx");
+        }
+        private void LoadExcelFile(string filename)
+        {
+            var sheetName = "Лист1";
+            DataTable table = new DataTable();
+            using (System.Data.OleDbConnection co = new System.Data.OleDb.OleDbConnection(
+                "Provider=Microsoft.ACE.OLEDB.12.0; " +
+                "Data Source='" + filename + "';" +
+                "Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1\""))
+
+            using (System.Data.OleDb.OleDbDataAdapter import =
+            new System.Data.OleDb.OleDbDataAdapter(
+            "select * from [" + sheetName + "$]", co))
+                import.Fill(table);
+        }
+    }
+}
---
+++ ImportExcel/ImportExcel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ImportExcel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImportExcel")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3c615c21-844d-43f5-9b29-ff7507c7cd5a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
